5c820dafe0beb2bbd09dfaa8680ebcbf5693b161
Brant Knudson <bknudson@us.ibm.com>
2016-06-22 10:44:10 -0500

    Add sample program for getting request ID

---
+++ request_id_hook.py
@@ -0,0 +1,37 @@
+# This sample shows how to set a hook on the session to get the request ID.
+# Requires: keystoneauth1, python-keystoneclient, requests
+
+
+import argparse
+
+from keystoneauth1.identity import v3
+from keystoneauth1 import session
+from keystoneclient.v3 import client
+import requests
+
+
+def log_request(r, *args, **kwargs):
+    print('%s %s' % (r.url, r.headers.get('x-openstack-request-id')))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--auth-url', default='http://localhost/identity')
+    parser.add_argument('--password')
+    args = parser.parse_args()
+
+    requests_session = requests.Session()
+    requests_session.hooks = {'response': log_request}
+
+    auth = v3.Password(auth_url='%s/v3' % args.auth_url,
+                       username='admin',
+                       password=args.password,
+                       project_name='admin',
+                       user_domain_name='Default',
+                       project_domain_name='Default')
+    ksa_session = session.Session(auth=auth, session=requests_session)
+    ks = client.Client(session=ksa_session)
+    ks.users.list()
+
+
+main()
