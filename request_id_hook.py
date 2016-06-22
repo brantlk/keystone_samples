79e6df68585826340179c977904c0cc5358b3c04
Brant Knudson <bknudson@us.ibm.com>
2016-06-22 11:04:27 -0500

    Don't need to create my own session for this.

--- request_id_hook.py
+++ request_id_hook.py
@@ -1,5 +1,5 @@
 # This sample shows how to set a hook on the session to get the request ID.
-# Requires: keystoneauth1, python-keystoneclient, requests
+# Requires: keystoneauth1, python-keystoneclient
 
 
 import argparse
@@ -7,7 +7,6 @@ import argparse
 from keystoneauth1.identity import v3
 from keystoneauth1 import session
 from keystoneclient.v3 import client
-import requests
 
 
 def log_request(r, *args, **kwargs):
@@ -21,16 +20,14 @@ def main():
     parser.add_argument('--password')
     args = parser.parse_args()
 
-    requests_session = requests.Session()
-    requests_session.hooks = {'response': log_request}
-
     auth = v3.Password(auth_url='%s/v3' % args.auth_url,
                        username='admin',
                        password=args.password,
                        project_name='admin',
                        user_domain_name='Default',
                        project_domain_name='Default')
-    ksa_session = session.Session(auth=auth, session=requests_session)
+    ksa_session = session.Session(auth=auth)
+    ksa_session.session.hooks = {'response': log_request}
     ks = client.Client(session=ksa_session)
     ks.users.list()
 
